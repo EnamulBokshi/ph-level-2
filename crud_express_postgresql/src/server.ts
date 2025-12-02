0ec62b51e42cd86a21ffdf22027e96ee443bd0bb
EnamulBokshi <enamulbokshi@gmail.com>
2025-12-02 23:23:26 +0600

    feat: restructure server and app files to improve organization and maintainability

--- crud_express_postgresql/src/server.ts
+++ crud_express_postgresql/src/server.ts
@@ -1,55 +1,6 @@
-import express, { NextFunction, Request, Response } from "express";
-
+import app from "./app";
 import config from "./config";
-import initDB, { pool } from "./config/db";
-import logger from "./middlewares/logger.middleware";
-import { userRoutes } from "./modules/users/users.routes";
-import { todoRoutes } from "./modules/todos/todos.routes";
-import authRoutes from "./modules/auth/auth.route";
-
-
-const app = express();
 const port = config.port;
-
-// For parsing the body
-app.use(express.json());
-
-// for form data
-app.use(express.urlencoded({ extended: true }));
-
-
-
-initDB();
-//Logger middleware
-
-
-
-//Health check api
-app.get("/health-check", logger,(req: Request, res: Response) => {
-	res.send("Cool! api is working");
-});
-
-// injecting user apis
-app.use("/users", userRoutes);
-
-
-
-
-// Todo apis
-
-app.use("/todos", todoRoutes);
-
-// Get a specific TODO:
-
-app.use('/auth', authRoutes); 
-// 404 route handling 
-app.use((req:Request, res:Response) =>{
-    res.status(404).json({
-        success: false,
-        message: 'Route not found',
-        path: req.path
-    })
-});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
