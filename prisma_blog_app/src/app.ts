03f60c0bc4d92a3fe2713a773e01cddaac74cc63
EnamulBokshi <enamulbokshi@gmail.com>
2026-01-02 22:17:41 +0600

    implement: better auth signup system and email verification

--- prisma_blog_app/src/app.ts
+++ prisma_blog_app/src/app.ts
@@ -1,9 +1,17 @@
 import express, {Application} from 'express'
 import { postRouter } from './modules/post/post.router';
+import cors from 'cors'
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 
 const app : Application = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:4000"
+}))
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use("/posts", postRouter);
 
